ff5c37a17fc8167cf4487f28c671e9525de66d7b
Fernando Senna <fmmsenna@gmail.com>
2024-10-08 17:27:13 -0300

    test of updating sync

--- src/pages/Welcome.jsx
+++ src/pages/Welcome.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 function Welcome() {
-  return <div>Welcome</div>;
+  const navigate = useNavigate();
+
+  return (
+    <div>
+      <h1>Welcome</h1>
+      <button onClick={() => navigate("/")}>Go back!</button>
+    </div>
+  );
 }
 
 export default Welcome;

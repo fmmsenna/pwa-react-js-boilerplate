d5c879a16ffd775db13466847ee95641eabc832c
Fernando Senna <fmmsenna@gmail.com>
2024-10-08 16:41:44 -0300

    version 2 test

--- src/version.js
+++ src/version.js
@@ -1,5 +1,5 @@
 export const VERSION = "0.0.1";
-export const BUILD_NUMBER = "1";
+export const BUILD_NUMBER = "2";
 export const RELEASE_TYPE = "beta"; // 'alpha', 'beta', 'rc', 'stable'
 
 export const getFullVersion = () =>
